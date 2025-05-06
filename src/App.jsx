ad7f3d9e241b1ae801870f6d2512cfcfa821aa32
Suhani_Gupta <suhu786.gupta@gmail.com>
2025-05-06 23:23:17 +0530

    seventh commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { WorkoutProvider } from './context/WorkoutContext';
+import HomePage from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <WorkoutProvider>
+      <div className="min-h-screen flex flex-col bg-gray-800">
+        <Navbar />
+        <div className="flex-grow container mx-auto px-4 py-8">
+          <HomePage />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </WorkoutProvider>
+  );
 }
 
-export default App
+export default App;
